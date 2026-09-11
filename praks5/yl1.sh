79454830fc3e876b9d9e3895f63ef6ddb15b891b
GridHouse <gridhouse34@gmail.com>
2026-09-11 10:27:02 +0300

    Update yl1.sh

---
+++ praks5/yl1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Sisesta oma vanus: " vanus
+
+if [ "$vanus" -ge 0 ] && [ "$vanus" -le 11 ]; then
+    echo "Oled laps"
+elif [ "$vanus" -ge 11 ] && [ "$vanus" -lt 18 ]; then
+    echo "Oled nooruk"
+elif [ "$vanus" -ge 18 ] && [ "$vanus" -lt 63 ]; then
+    echo "Oled täiskasvanu"
+elif [ "$vanus" -ge 63 ]; then
+    echo "Oled seenior"
+else
+    echo "Vigane vanus"
+fi
